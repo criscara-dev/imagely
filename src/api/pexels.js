3cd8a518bfb947ff0da490b1d3e6e3acb83b3315
Cristian Caratti <cristian.caratti.xx@gmail.com>
2019-09-15 12:08:32 +0100

    removed Auth Pexels for GitHub

--- src/api/pexels.js
+++ src/api/pexels.js
@@ -3,6 +3,6 @@ import axios from 'axios';
 export default axios.create({
   baseURL: 'https://api.pexels.com',
   headers: {
-    Authorization: '563492ad6f91700001000001c1dbb9199ea246bdbd6faac3521cb0fd'
+    Authorization: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
   }
 });
